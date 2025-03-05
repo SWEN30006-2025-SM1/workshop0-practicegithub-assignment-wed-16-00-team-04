d5da93eb0a79d2dfc5852a9bdf3c73f3a46442c7
Himesh Sonal Weerawardane <hweerawardan@student.unimelb.edu.au>
2025-03-05 16:58:26 +1100

    Himesh's dummy commit

--- app/src/main/java/example/Calculator.java
+++ app/src/main/java/example/Calculator.java
@@ -3,21 +3,30 @@ package example;
 public class Calculator {
     public int add(int a, int b) {
         // TODO: Implement this method
-        return 4;
+        int sum = a + b;
+        return sum;
     }
 
     public int minus(int a, int b) {
         // TODO: Implement this method
-        return 10;
+        int sub = a - b;
+        return sub;
     }
 
     public int multiply(int a, int b) {
         // TODO: Implement this method
-        return 10;
+        int multi = a * b;
+        return multi;
     }
 
     public double divide(int a, int b) {
         // TODO: Implement this method
-        return 5;
+        int div;
+        if (a == 0) {
+            return 0;
+        } else {
+            div = a / b;
+        }
+        return div;
     }
 }
